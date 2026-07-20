79658b24e5bb91bb66bd4b3998bfff4951458f99
Federico_Cravero <fedecrave@hotmail.com>
2026-07-20 11:34:58 -0300

    Add files via upload

---
+++ RFID/RFID.ino
@@ -0,0 +1,41 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+//pines de arduino
+//SDA--10 
+//SDK--13
+//MOSI-11
+//MISO-12
+//IRQ--no conectado
+//RST--9
+
+#define RST_PIN  9    //Pin 9 para el reset del RC522
+#define SS_PIN  10   //Pin 10 para el SS (SDA) del RC522
+MFRC522 mfrc522(SS_PIN, RST_PIN); //Creamos el objeto para el RC522
+
+void setup() {
+  Serial.begin(9600); //Iniciamos la comunicación  serial
+  SPI.begin();        //Iniciamos el Bus SPI
+  mfrc522.PCD_Init(); // Iniciamos  el MFRC522
+  Serial.println("Lectura del UID");
+}
+
+void loop() {
+  // Revisamos si hay nuevas tarjetas  presentes
+  if ( mfrc522.PICC_IsNewCardPresent()) 
+        {  
+      //Seleccionamos una tarjeta
+            if ( mfrc522.PICC_ReadCardSerial()) 
+            {
+                  // Enviamos serialemente su UID
+                  Serial.print("Card UID:");
+                  for (byte i = 0; i < mfrc522.uid.size; i++) {
+                          Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+                          Serial.print(mfrc522.uid.uidByte[i], HEX);   
+                  } 
+                  Serial.println();
+                  // Terminamos la lectura de la tarjeta  actual
+                  mfrc522.PICC_HaltA();         
+            }      
+  } 
+}
